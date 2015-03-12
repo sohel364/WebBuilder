40189f779f9b4eb08ee0527a417b6e2f88590b4d
SWORD <abu.saif01@gmail.com>
2015-03-13 02:13:39 +0600

    follow up commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	$dirs=scandir("./templates");
 	echo "Available Tempalates<br/>";
-	print_r($dirs);
+
 	for($i=2;$i<sizeof($dirs);$i++)
 	{
 		echo '<a href="./manager/content_manager/content_manager.php?template='.$dirs[$i].'">'.$dirs[$i].'</a> <br/>';
