366e6a4d428f0b746ab6c20ee7e57296f8a7a255
sohel364 <sohel.official@gmail.com>
2015-04-10 01:19:52 +0600

    Renaming Object Files

---
+++ objects/ObjMenu.php
@@ -0,0 +1,10 @@
+<?php 
+class Menu {
+	private $_menu_id;
+	private $_menu_title;
+	private $_has_submenue;
+	
+	private $_sub_menu_id;
+	private $_content_id;
+}
+?>
---
+++ objects/ObjSubMenu.php
@@ -0,0 +1,10 @@
+<?php 
+	class SubMenu{
+		private $_submenu_id;
+		private $_submenu_title;
+		
+		public  setSubMenuID(){
+			
+		}
+	}
+?>
