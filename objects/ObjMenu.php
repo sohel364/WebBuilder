d3f1f7044b7942b598bb8a48f3c404546af1773f
sohel364 <sohel.official@gmail.com>
2015-04-10 03:35:40 +0600

    Base class are created

--- objects/ObjMenu.php
+++ objects/ObjMenu.php
@@ -4,7 +4,29 @@ class Menu {
 	private $_menu_title;
 	private $_has_submenue;
 	
-	private $_sub_menu_id;
-	private $_content_id;
+	public function setMenuID($value){
+		$this->_menu_id=$value;
+	}
+	public function getMenuID(){
+		return $this->_menu_id;
+	}
+	
+	public function setMenuTitle($value){
+		$this->_menu_title=$value;	
+	}
+	public function getMenuTitle(){
+		return $this->_menu_title;
+	}
+	
+	public function setHasSubmenu($isTrue){
+		$this->_has_submenue=$isTrue;
+	}
+	public function getHasSubMenu(){
+		return $this->_has_submenue;
+	}
+	
+	public function __construct(){
+		
+	}
 }
 ?>
--- objects/ObjSubMenu.php
+++ objects/ObjSubMenu.php
@@ -3,8 +3,31 @@
 		private $_submenu_id;
 		private $_submenu_title;
 		
-		public  setSubMenuID(){
-			
+		private $_menuID;
+				
+		public function setSubMenuID($value){
+			$this->_submenu_id=$value;
+		}
+		public function getSubMenuID(){
+			return $this->_submenu_id;
+		}
+		
+		public function setSubMenuTitle($value){
+			$this->_submenu_title=$value;
+		}
+		public function getSubMenuTitle(){
+			return $this->_submenu_title;
+		}
+		
+		public function setMenuID($value){
+			$this->_menuID=$value;
+		}
+		public function getMenuID(){
+			return $this->_menuID;
+		}
+		
+		public function __construct(){
+					
 		}
 	}
 ?>
