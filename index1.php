3a1eee564e4722cb1011ce807c69125ffdbc5640
ShrabonMohsin <Mohsin.ofcl@gmail.com>
2015-04-18 22:27:26 +0600

    Files added for the new template integration index1.php is the working template.
    @ sohel vai : Please use it for main projet
    i will keep you updating here
    
    Signed-off-by: ShrabonMohsin <Mohsin.ofcl@gmail.com>

--- index1.php
+++ index1.php
@@ -1,22 +1,21 @@
-<?php
-session_start( );
-?>
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Web Builder</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	
-</head>
-<body>
-<div id="container">
+    <title>Web Builder</title>
 
-<nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container-fluid">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+		<link href="css/bootstrap-combined.min.css" rel="stylesheet" />
+		<link href="css/main.cs" rel="stylesheet" />
+		
+  </head>
+  <body>
+		<div class="container">
+		    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
@@ -34,113 +33,89 @@ session_start( );
 						<li><a href="#">Notes</a></li>
 						<li><a href="#">Others</a></li>
           </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <?php 
-			//echo $_SESSION['name']."  ".$_SESSION['isLogged'];
-		
-			if(isset($_SESSION['isLogged']))
-			{
-				?>
-				<li><a href="./mypage.php">My Page</a></li>
-				<span ><?php echo $_SESSION['name'] ?></span> 
-		<?php 
-			}
-			else  echo '<li><a href="./manager/user_manager/login.php">Login</a></li> ';
-		?>
-          </ul>
+          <ul class="nav navbar-nav pull-right">
+					<li class="dropdown">
+						<a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
+						<div class="dropdown-menu" style="padding: 15px; padding-bottom: 5pxpx;">
+							<form method="post" action="login" accept-charset="UTF-8">
+								<input style="margin-bottom: 15px;" type="text" placeholder="Username" id="username" name="username">
+								<input style="margin-bottom: 15px;" type="password" placeholder="Password" id="password" name="password">
+								<input style="float: left; margin-right: 10px;" type="checkbox" name="remember-me" id="remember-me" value="1">
+								<label class="string optional" for="user_remember_me"> Remember me</label>
+								<input class="btn btn-primary btn-block" type="submit" id="sign-in" value="Sign In">
+							
+							</form>
+						</div>
+					</li>
+				</ul>
         </div><!--/.nav-collapse -->
       </div>
     </nav>
- 
- <!--<div id="top_pl">
-
- 	 <div>
-		<table style="margin-left: 64px; display: inline-table">
-			<tr>
-				<td><a href="#">Home</a></td>
-				<td><a href="#">Account</a></td>
-				<td><a href="#">Billing</a></td>
-				<td><a href="#">Notes</a></td>
-				<td><a href="#">Others</a></td>
-			</tr>
-		</table>
-		<div style="float: right;">
-		<?php 
-			//echo $_SESSION['name']."  ".$_SESSION['isLogged'];
-		
-			if(isset($_SESSION['isLogged']))
-			{
-				?>
-				<li><a href="./mypage.php">My Page</a></li>
-				<span ><?php echo $_SESSION['name'] ?></span> 
-		<?php 
-			}
-			else  echo '<li><a href="./manager/user_manager/login.php">Login</a></li> ';
-		?>
-		
-		</div>
-	</div> 
- </div>-->
-<div> 
-	 <div id="template_brief">
-			 	
-			<h2>Pick your template, Build your own site</h2>
-			<p>Web Based Template builder 
-			with Simple steps</p>
-			<p><ul>Steps</ul></p>
-			<ul>
-				<li>1. Register</li>
-				<li>2. Choose Categorized template</li>
-				<li>3. Build your home with us !</li>
-			</ul>
-	 </div>
-	  <div id="template_slide_item">
-	  	<p style=" font-size: 40px;
-    	text-align: center;">Template-1</p>
+			
+			<div class="row" style="padding-top:50px;">
+							<div class="col-sm-3">
+								<h4 class="well" style="padding:5px;">Chose your Category</h4>
+								<div class="panel-group" style="padding-top:0px;" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" style="text-decoration: none;">
+          <span class="glyphicon glyphicon-minus"></span>
+          Collapsible Group Item #1
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
       </div>
-      
-      <br/>
-      <br/>
- </div>
-
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" style="text-decoration: none;">
+          <span class="glyphicon glyphicon-plus"></span>
+          Collapsible Group Item #2
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" style="text-decoration: none;">
+          <span class="glyphicon glyphicon-plus"></span>
+          Collapsible Group Item #3
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
 </div>
-<br/><br/>
 
-	
-	
-<?php
-	$dirs=scandir("./templates");
-	//echo "Available Tempalates<br/>";
-	//print_r($dirs);
-	for($i=2;$i<sizeof($dirs);$i++)
-	{		
-?>
-	<div class="templat_block">
-	<p><ul><?php echo $dirs[$i] ?></ul></p>
-	<?php 
-		$templates=scandir("./templates/$dirs[$i]");
-		for($j=2; $j<sizeof($templates);$j++)
-		{
-		?>
-			<div class="tp_pv">
-			<a href="./manager/content_manager/template_editor.php?
-			category=<?php echo $dirs[$i] ?>&template=<?php echo $templates[$j] ?>" >
-				<?php echo $templates	[$j] ?>
-			</a> </div>
-		<?php 
-		}
-	?>
-	
-	
-	
-	</div>
-<?php	
-	} 
-?>
+							</div>
+							
+						</div>
+		
 		
 
+    </div>
 
-
-
-
-</body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+		<script src="js/MenuDropDown.js"></script>
+		<script src="js/CatPanel.js"></script>
+  </body>
+</html>
