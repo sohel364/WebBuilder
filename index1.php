1d320c76dac79f8e9f17de5a400e72cb1a92c1ea
ShrabonMohsin <Mohsin.ofcl@gmail.com>
2015-04-19 00:14:29 +0600

    sign in box size problem fixed. Search box added index1.php file updated.

--- index1.php
+++ index1.php
@@ -47,6 +47,15 @@
 							</form>
 						</div>
 					</li>
+						<li>
+							<form class="navbar-form navbar-left" role="search">
+								<div class="input-group">
+									<input type="text" class="form-control" placeholder="Search for...">
+      						<span class="input-group-btn">
+        					<button class="btn btn-default" type="button">Go!</button>
+      						</span>
+								</div>
+      </form></li>
 				</ul>
         </div><!--/.nav-collapse -->
       </div>
