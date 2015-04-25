a050ea348f00ec2d7d78c6952c81b1c7d97b8235
Tamal Tahsin <t.tahasin@gmail.com>
2015-04-25 19:03:01 +0600

    content_manager/content_manager.php sample

--- manager/content_manager/content_manager.php
+++ manager/content_manager/content_manager.php
@@ -1,7 +1,18 @@
 <?php
+include_once '../dataAccess/databaseHelper.php';
 
-	function InsertContent($data) {
-		echo $data;
+class ContentPageManager{
+	
+	function SavePageContent($ObjContentArry) {
+		//$sqlInnerData='INSERT INTO  `jd_entertainment_op_date_time` (`days`, `duration`, `id`) VALUES ("'.safe($exploded[0]).'","'.safe($exploded[1]).'","'.safe($tmpID).'")';
+		$sqlContent='INSERT INTO `webbuilder`.`content` (`content_id`, `content_html`, `isMenu`, `content_menu_id`) VALUES (NULL, '<div>Menu Item COntent</div>', '1', '2')';
+		$dbHelper=new databaseHelper();
+		$dbHelper->ExecuteInsertReturnID($sqlContent);
 	}
-	 
+	
+	function SavePageContent($ObjContentArry) {
+		;
+	}
+	
+}
 ?>
