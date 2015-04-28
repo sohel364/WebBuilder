81692961e8d3e58a47e17f0b28f228ecb877464e
sohel364 <sohel.official@gmail.com>
2015-04-28 23:54:32 +0600

    user_manager.php for user registration and user data related activity

---
+++ manager/user_manager/user_manager.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * File Name : user_manual.php
+ * Description : This Files contains most of the user related tasks
+ * Usage : 
+ * 1. User Registration
+ * 2. User Verificaiton
+ * 3. User Administration
+ * 4. User Related Offer Management
+ * 
+ * Created on : 28.04.2015
+ * Last Modified :
+ * Change/Update Note :
+ * 
+ *  
+ */ 
+
+class UserManager{
+	
+}
+
+
+?>
