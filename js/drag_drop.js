db23c143e711580b3a2cca496016cd170b60d3f2
Jisan Mahmud <jisanmahmud@yahoo.com>
2015-07-26 00:38:12 +0600

    Dialog Close event added
    Change-Id: Ieb6ddb1b92d2b5e4b93c4ee05948eb62b9e065ea
    Signed-off-by: Jisan Mahmud <jisanmahmud@yahoo.com>

--- js/drag_drop.js
+++ js/drag_drop.js
@@ -99,127 +99,143 @@ $(function() {
 
 		}
 	});
+	
+	function showButtonEditPanel() {
+		$("#btn_text").val('');
+		$("#btn_link").val('');
+		$("#btn_dialog_close").click(function(){
+			$("#btn_edit_dialog").dialog("close");
+		});
+		
+		$("#btn_dialog_save").click(function(){
+			
+			/*var btn_txt = $("#btn_text").value();
+			child_item.css("text", btn_txt);*/
+			
+			alert("save " + btn_txt);
+			$("#btn_edit_dialog").dialog("close");
+		});
+		
+		$("#btn_edit_dialog").dialog({
+			dialogClass : "no-close",
+			resizable : false,
+			draggable : true,
+			closeOnEscape : true,
+			title : "Button Edit Panel",
+			width : 250,
+			show : {
+				effect : "slide",
+				duration : 200,
+				direction : "up"
+			},
+		/*
+		 * hide : { effect : "slide", duration : 200 }, position : { my : "right
+		 * top", at : "right top", of : $(".droppedFields") },
+		 */
+
+		});
+
+		$("#color_picker")
+				.spectrum(
+						{
+							color : "#6cc8f9",
+							flat : false,
+							showInput : false,
+							containerClassName : 'color_picker_container',
+							replacerClassName : 'color_picker_icon',
+							showInitial : true,
+							showPalette : true,
+							showSelectionPalette : true,
+							maxPaletteSize : 5,
+							preferredFormat : "hex",
+							localStorageKey : "spectrum.demo",
+							move : function(color) {
+
+							},
+							show : function() {
+
+							},
+							beforeShow : function() {
+							},
+							hide : function() {
+							},
+							change : function(color) {
+								child_item.css("background", color);
+							},
+							palette : [
+									[ "rgb(0, 0, 0)", "rgb(67, 67, 67)",
+											"rgb(102, 102, 102)",
+											"rgb(204, 204, 204)",
+											"rgb(217, 217, 217)",
+											"rgb(255, 255, 255)" ],
+									[ "rgb(152, 0, 0)", "rgb(255, 0, 0)",
+											"rgb(255, 153, 0)",
+											"rgb(255, 255, 0)",
+											"rgb(0, 255, 0)",
+											"rgb(0, 255, 255)",
+											"rgb(74, 134, 232)",
+											"rgb(0, 0, 255)",
+											"rgb(153, 0, 255)",
+											"rgb(255, 0, 255)" ],
+									[ "rgb(230, 184, 175)",
+											"rgb(244, 204, 204)",
+											"rgb(252, 229, 205)",
+											"rgb(255, 242, 204)",
+											"rgb(217, 234, 211)",
+											"rgb(208, 224, 227)",
+											"rgb(201, 218, 248)",
+											"rgb(207, 226, 243)",
+											"rgb(217, 210, 233)",
+											"rgb(234, 209, 220)",
+											"rgb(221, 126, 107)",
+											"rgb(234, 153, 153)",
+											"rgb(249, 203, 156)",
+											"rgb(255, 229, 153)",
+											"rgb(182, 215, 168)",
+											"rgb(162, 196, 201)",
+											"rgb(164, 194, 244)",
+											"rgb(159, 197, 232)",
+											"rgb(180, 167, 214)",
+											"rgb(213, 166, 189)",
+											"rgb(204, 65, 37)",
+											"rgb(224, 102, 102)",
+											"rgb(246, 178, 107)",
+											"rgb(255, 217, 102)",
+											"rgb(147, 196, 125)",
+											"rgb(118, 165, 175)",
+											"rgb(109, 158, 235)",
+											"rgb(111, 168, 220)",
+											"rgb(142, 124, 195)",
+											"rgb(194, 123, 160)",
+											"rgb(166, 28, 0)",
+											"rgb(204, 0, 0)",
+											"rgb(230, 145, 56)",
+											"rgb(241, 194, 50)",
+											"rgb(106, 168, 79)",
+											"rgb(69, 129, 142)",
+											"rgb(60, 120, 216)",
+											"rgb(61, 133, 198)",
+											"rgb(103, 78, 167)",
+											"rgb(166, 77, 121)",
+											"rgb(91, 15, 0)", "rgb(102, 0, 0)",
+											"rgb(120, 63, 4)",
+											"rgb(127, 96, 0)",
+											"rgb(39, 78, 19)",
+											"rgb(12, 52, 61)",
+											"rgb(28, 69, 135)",
+											"rgb(7, 55, 99)",
+											"rgb(32, 18, 77)",
+											"rgb(76, 17, 48)" ] ]
+						});
+		
+		
+	}
 
 	function showEditPanel() {
 		editable_control = $("#" + clicked_dropped_item_id);
 
 		if (clicked_dropped_item_id.search('button') == 0) {
-			/* alert("button : " + clicked_dropped_item_id); */
-			$("#btn_text").val('');
-			$("#btn_link").val('');
-
-			$("#btn_edit_dialog").dialog({
-				dialogClass : "no-close",
-				resizable : false,
-				draggable : true,
-				closeOnEscape : true,
-				title : "Button Edit Panel",
-				width : 250,
-				show : {
-					effect : "slide",
-					duration : 200,
-					direction : "up"
-				},
-			/*
-			 * hide : { effect : "slide", duration : 200 }, position : { my :
-			 * "right top", at : "right top", of : $(".droppedFields") },
-			 */
-
-			});
-
-			$("#color_picker")
-					.spectrum(
-							{
-								color : "#6cc8f9",
-								flat : false,
-								showInput : false,
-								containerClassName : 'color_picker_container',
-								replacerClassName: 'color_picker_icon',
-								showInitial : true,
-								showPalette : true,
-								showSelectionPalette : true,
-								maxPaletteSize : 5,
-								preferredFormat : "hex",
-								localStorageKey : "spectrum.demo",
-								move : function(color) {
-
-								},
-								show : function() {
-
-								},
-								beforeShow : function() {
-								},
-								hide : function() {
-								},
-								change : function(color) {
-									child_item.css("background", color);
-								},
-								palette : [
-										[ "rgb(0, 0, 0)", "rgb(67, 67, 67)",
-												"rgb(102, 102, 102)",
-												"rgb(204, 204, 204)",
-												"rgb(217, 217, 217)",
-												"rgb(255, 255, 255)" ],
-										[ "rgb(152, 0, 0)", "rgb(255, 0, 0)",
-												"rgb(255, 153, 0)",
-												"rgb(255, 255, 0)",
-												"rgb(0, 255, 0)",
-												"rgb(0, 255, 255)",
-												"rgb(74, 134, 232)",
-												"rgb(0, 0, 255)",
-												"rgb(153, 0, 255)",
-												"rgb(255, 0, 255)" ],
-										[ "rgb(230, 184, 175)",
-												"rgb(244, 204, 204)",
-												"rgb(252, 229, 205)",
-												"rgb(255, 242, 204)",
-												"rgb(217, 234, 211)",
-												"rgb(208, 224, 227)",
-												"rgb(201, 218, 248)",
-												"rgb(207, 226, 243)",
-												"rgb(217, 210, 233)",
-												"rgb(234, 209, 220)",
-												"rgb(221, 126, 107)",
-												"rgb(234, 153, 153)",
-												"rgb(249, 203, 156)",
-												"rgb(255, 229, 153)",
-												"rgb(182, 215, 168)",
-												"rgb(162, 196, 201)",
-												"rgb(164, 194, 244)",
-												"rgb(159, 197, 232)",
-												"rgb(180, 167, 214)",
-												"rgb(213, 166, 189)",
-												"rgb(204, 65, 37)",
-												"rgb(224, 102, 102)",
-												"rgb(246, 178, 107)",
-												"rgb(255, 217, 102)",
-												"rgb(147, 196, 125)",
-												"rgb(118, 165, 175)",
-												"rgb(109, 158, 235)",
-												"rgb(111, 168, 220)",
-												"rgb(142, 124, 195)",
-												"rgb(194, 123, 160)",
-												"rgb(166, 28, 0)",
-												"rgb(204, 0, 0)",
-												"rgb(230, 145, 56)",
-												"rgb(241, 194, 50)",
-												"rgb(106, 168, 79)",
-												"rgb(69, 129, 142)",
-												"rgb(60, 120, 216)",
-												"rgb(61, 133, 198)",
-												"rgb(103, 78, 167)",
-												"rgb(166, 77, 121)",
-												"rgb(91, 15, 0)",
-												"rgb(102, 0, 0)",
-												"rgb(120, 63, 4)",
-												"rgb(127, 96, 0)",
-												"rgb(39, 78, 19)",
-												"rgb(12, 52, 61)",
-												"rgb(28, 69, 135)",
-												"rgb(7, 55, 99)",
-												"rgb(32, 18, 77)",
-												"rgb(76, 17, 48)" ] ]
-							});
+			showButtonEditPanel();
 
 		} else if (clicked_dropped_item_id.search('textarea') == 0) {
 			alert("textarea : " + clicked_dropped_item_id);
