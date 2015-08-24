8d0de923401d2bb909d0ae2b3e01d2ed3b9f4225
sohel364 <sohel.official@gmail.com>
2015-08-24 21:41:32 +0600

    Header Contents

--- common_html_headercontent.php
+++ common_html_headercontent.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Web Builder</title>	
-	<script type="text/javascript" src="../../../WebBuilder/js/main.js"></script>
+	<script type="text/javascript" src="../../../webbuilder/js/main.js"></script>
     <!-- Bootstrap -->
-    <link href="../../../WebBuilder/css/bootstrap.min.css" rel="stylesheet" />
-	<link href="../../../WebBuilder/css/bootstrap-combined.min.css" rel="stylesheet" />
-	<link href="../../../WebBuilder/css/overlay-bootstrap.css" rel="stylesheet" />
-	<link href="../../../WebBuilder/css/overlay-bootstrap.min.css" rel="stylesheet" />
+    <link href="../../../webbuilder/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="../../../webbuilder/css/bootstrap-combined.min.css" rel="stylesheet" />
+	<link href="../../../webbuilder/css/overlay-bootstrap.css" rel="stylesheet" />
+	<link href="../../../webbuilder/css/overlay-bootstrap.min.css" rel="stylesheet" />
   </head>
 <body>
