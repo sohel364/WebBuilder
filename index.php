e2f30cc7e3d290f6f712cc7f383810dbd86fe9f8
sohel364 <sohel.official@gmail.com>
2015-08-28 22:53:12 +0600

    Changes for Adapting 4 Folder in the Template Tree It needs to be made Dynamic @ Tamal

--- index.php
+++ index.php
@@ -26,6 +26,8 @@
 		$dirs=scandir("./templates");
 		//echo "Available Tempalates<br/>";
 		//print_r($dirs);
+		
+		//return;
 		for($i=2;$i<sizeof($dirs);$i++)
 		{		
 ?>
@@ -33,7 +35,7 @@
 	  <div class="panel panel-default">
 		<div class="panel-heading">
 		  <h4 class="panel-title">
-			<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php if($i==2){echo "One";} else {echo "Two";} ?>" style="text-decoration: none;">
+			<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php if($i==2){echo "One";} else if($i==3) {echo "Two";} else if($i==4) {echo "Three";} else if($i==5) {echo "Four";} ?>" style="text-decoration: none;">
 			  <?php if($i==2){?>
 			  <span class="glyphicon glyphicon-minus"></span>
 			  <?php } else {?>
@@ -43,7 +45,9 @@
 		  </h4>
 		</div>
 			
-		<div id="collapse<?php if($i==2){ echo "One";} else { echo "Two";}?>" class="panel-collapse <?php if($i==2) echo "collapse in";else echo "collapse"?>" >
+		<div id="collapse<?php if($i==2){ echo "One";} else if($i==3) 
+		{ echo "Two";} else if($i==4) { echo "Three";} else if($i==5) 
+		{ echo "Four";}?>" class="panel-collapse <?php if($i==2) echo "collapse in";else echo "collapse"?>" >
 		<?php 
 		$templates=scandir("./templates/$dirs[$i]");	
 		for($j=2; $j<sizeof($templates);$j++)
