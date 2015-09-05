af525f41c93acb82d3f00f3083b3c666edffcf60
Tamal Tahsin <t.tahasin@gmail.com>
2015-09-05 22:51:37 +0600

    Removed virtual form for uploading image

--- js/savePage.js
+++ js/savePage.js
@@ -7,27 +7,6 @@
 // onload functionalities
 $(document).ready(function() {
     hideSavingIcon();
-    $("#uploadimage").on('submit', (function (e) {
-        e.preventDefault();
-        
-        $.ajax({
-            url: getBaseUrl()+'/views/content_views/savePage.php', // Url to which the request is send
-            type: "POST", // Type of request to be send, called as method
-            data: new FormData(this), // Data sent to server, a set of key/value pairs (i.e. form fields and values)
-            contentType: false, // The content type used when sending data to the server.
-            cache: false, // To unable request pages to be cached
-            processData: false, // To send DOMDocument or non processed data file it is set to false
-            success: function (data)   // A function to be called if request succeeds
-            {
-                
-            },
-            error: function(xhr, status, error) {
-                hideSavingIcon();
-                var err =  xhr.responseText;
-                alert(err);
-            }
-        });
-    }));
 });
 
 
