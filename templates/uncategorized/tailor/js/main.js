e96e571521cf5732745aabc4890391e7deb3f8d0
Jisan Mahmud <jisanmahmud@yahoo.com>
2015-10-11 19:28:13 +0600

    Tailor Template script file modified
    Change-Id: I994fe7d393e752de6154289dc1204b8709ef52d3
    Signed-off-by: Jisan Mahmud <jisanmahmud@yahoo.com>

--- templates/uncategorized/tailor/js/main.js
+++ templates/uncategorized/tailor/js/main.js
@@ -16,47 +16,4 @@ $(window).load(function(){
 
 function onTemplateMenuLoad(){
 	console.log("onTemplateMenuLoad Clicked");
-	startImageSlider("all");
-}
-
-function startImageSlider(slider_id){
-
-	console.log("InitSlider Called for " + slider_id);
-	
-	var visible_items = $("#flexiselDemo3").data("visible_items");
-	var animation_speed = $("#flexiselDemo3").data("animation_speed");
-	var pause_time = $("#flexiselDemo3").data("pause_time");
-	
-//	alert(visible_items + " : " + animation_speed + " : " + pause_time );
-	
-	
-	
-	if (slider_id == "all" || slider_id == "flexiselDemo3")
-	
-	{
-		console.log("resetting for flexiselDemo3 ");
-		
-		$("#flexiselDemo3").flexisel({
-	        visibleItems: visible_items,
-	        animationSpeed: animation_speed,
-	        autoPlay: true,
-	        autoPlaySpeed: pause_time,            
-	        pauseOnHover: true,
-	        enableResponsiveBreakpoints: true,
-	        responsiveBreakpoints: { 
-	            portrait: { 
-	                changePoint:480,
-	                visibleItems: 1
-	            }, 
-	            landscape: { 
-	                changePoint:640,
-	                visibleItems: 2
-	            },
-	            tablet: { 
-	                changePoint:768,
-	                visibleItems: 3
-	            }
-	        }
-	    });
-	}
 }
