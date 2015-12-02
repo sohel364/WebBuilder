4c1fb27a94e31ced70cf0ac00fca1ace4056ac79
sohel364 <sohel.official@gmail.com>
2015-12-02 08:54:06 +0600

    image upload,remove,delete status and view code for media_manager.php

--- manager/content_manager/media_manager.php
+++ manager/content_manager/media_manager.php
@@ -11,6 +11,27 @@ class MediaManager {
         $dbHelper = new databaseHelper();
         $dbHelper->ExecuteInsertReturnID($sqlContent);
     }
+    
+    /*
+     * Gets all media contents by template ID
+     */
+    public function GetMediaByID($key){
+        $sqlContent = 'SELECT `id`,`res_name`,`type`,`status`,`user_id`,`template_id` FROM `media_arch` WHERE template_id = \''.$key.'\'';
+        $dbHelper = new databaseHelper();
+        $dbHelper->ExecuteInsertReturnID($sqlContent);
+    }
+    
+    /*
+     * Update Media Content Status by key
+     * Pass Status if needed, by default it's 0
+     * 0 means not published
+     * 1 means published
+     */
+    public function UpdateMediaByID($key,$status="0"){        
+        $sqlContent = 'UPDATE `webbuilder`.`media_arch` SET `status` = \''.$status.'\' WHERE `media_arch`.`id` = \''.$key.'\'';        
+        $dbHelper = new databaseHelper();
+        $dbHelper->ExecuteInsertReturnID($sqlContent);
+    }
 }
     
 ?>
